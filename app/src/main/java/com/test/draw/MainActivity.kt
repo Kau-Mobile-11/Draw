186434c9e34bb1b21178522f579fb2990624d2e8
youngjin <gojib2002@naver.com>
2019-09-23 14:11:28 +0900

    start

---
+++ app/src/main/java/com/test/draw/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.test.draw
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var canvasView: CanvasView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        canvasView = findViewById(R.id.canvas)
+
+//        clearCanvas.setOnClickListener(ClearCanvas(canvasView) as View.OnClickListener)
+
+    }
+
+//    fun ClearCanvas(view: View) {
+//        canvasView.ClearCanvas()
+//    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId) {
+            R.id.erase -> canvasView.ClearCanvas()
+        }
+
+        return super.onOptionsItemSelected(item)
+
+    }
+
+}
