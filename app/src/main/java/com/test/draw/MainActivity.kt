32da2264077c0843819d8e5e6a636a109f3a550b
leguyong <leguyong@gmail.com>
2019-10-16 13:38:37 +0900

    test

--- app/src/main/java/com/test/draw/MainActivity.kt
+++ app/src/main/java/com/test/draw/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         canvasView = findViewById(R.id.canvas)
-
+// hi
 //        clearCanvas.setOnClickListener(ClearCanvas(canvasView) as View.OnClickListener)
 
     }
