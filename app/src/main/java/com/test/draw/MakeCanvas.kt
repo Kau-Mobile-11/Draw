a1bef974afe64ce301a52e27053ffcbaf7a96707
gojib2002 <gojib2002@naver.com>
2019-10-16 13:25:11 +0900

    그림 그리기
    firebase에 있는 DB를 이용해서 그림 그리기.

--- app/src/main/java/com/test/draw/MakeCanvas.kt
+++ app/src/main/java/com/test/draw/MakeCanvas.kt
@@ -1,10 +1,14 @@
 package com.test.draw
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import com.google.firebase.database.*
+
 
 class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(context, attrs) {
 
@@ -14,6 +18,12 @@ class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(co
     private var mPaint: Paint = Paint()
     private var mX : Float = 0.toFloat()
     private var mY : Float = 0.toFloat()
+    private var Finished : Boolean = true
+    private var arrivedX : Boolean = false
+    private var arrivedY : Boolean = false
+    val database : FirebaseDatabase = FirebaseDatabase.getInstance()  // firebase db의 인스턴스를 가져옴
+
+
 
     init {
         mPaint.isAntiAlias = true
@@ -26,7 +36,9 @@ class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(co
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas!!.drawPath(mPath, mPaint)
+
     }
+
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -36,13 +48,18 @@ class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(co
 
     }
 
-    private fun onStartTouchEvent(x:Float, y:Float) {
+    private fun onStartTouchEvent(x: Float, y:Float) {
         mPath.moveTo(x,y)
-        mX = x
-        mY = y
+        mX=x
+        mY=y
+        Finished = false;
+
+        //Log.d(TAG,"START")
     }
 
     private fun onMoveTouchEvent(x:Float, y:Float) {
+        if(Finished) return;
+
         val dx = Math.abs(x - mX)
         val dy = Math.abs(y - mY)
         if(dx >= TOLERANCE || dy >= TOLERANCE) {
@@ -50,11 +67,15 @@ class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(co
             mX = x
             mY = y
         }
-
     }
 
     private fun upTouchEvent(){
-        mPath.lineTo(mX,mY)
+        //mPath.lineTo(mX,mY)
+        Finished = true
+        arrivedX = false;
+        arrivedY = false;
+
+        //Log.d(TAG,"FINISH")
     }
 
     fun ClearCanvas(){
@@ -63,30 +84,61 @@ class CanvasView(internal var context: Context, attrs : AttributeSet?) : View(co
     }
 
 
-
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val x = event.x
         val y = event.y
 
-        when(event.action) {
+        //database.getReference("x").push().setValue(x) //  push()를 쓰면 누적 저장
+        //database.getReference("y").push().setValue(y) //  위에 안쓴거는 계속 갱신
+
+        database.getReference("path").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                var value = p0.value as Map<String, String>;
+
+                if(value.getValue("FIN").equals("T")) {
+                    upTouchEvent()
+                    invalidate()
+                }else if(value.getValue("START").equals("T")){
+                    onStartTouchEvent(value.getValue("X").toFloat(), value.getValue("Y").toFloat())
+                    invalidate()
+                }else{
+                    onMoveTouchEvent(value.getValue("X").toFloat(), value.getValue("Y").toFloat())
+                    invalidate()
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
+        when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                onStartTouchEvent(x,y)
-                invalidate()
+//                onStartTouchEvent(x, y)
+//                invalidate()
+                database.getReference("path").setValue(mapOf("X" to x.toFloat().toString(), "Y" to y.toFloat().toString(), "FIN" to "F", "START" to "T"))
             }
             MotionEvent.ACTION_MOVE -> {
-                onMoveTouchEvent(x,y)
-                invalidate()
+//                onMoveTouchEvent(x, y)
+//                invalidate()
+
+                database.getReference("path").setValue(mapOf("X" to x.toFloat().toString(), "Y" to y.toFloat().toString(), "FIN" to "F", "START" to "F"))
             }
             MotionEvent.ACTION_UP -> {
-                upTouchEvent()
-                invalidate()
+//                upTouchEvent()
+//                invalidate()
+                database.getReference("path").setValue(mapOf("X" to x.toFloat().toString(), "Y" to y.toFloat().toString(), "FIN" to "T", "START" to "F"))
             }
         }
+
         return true
     }
+
 
     companion object {
         private val TOLERANCE = 5f
     }
 
 }
+
